96e65df02bead0c8a0deb7ed4dc14c62fe6a44a1
Lino Lerch <lerch.lino@gmail.com>
2023-01-02 18:27:18 +0100

    Evaluation: Robustness Benchmark slurm scripts

---
+++ tools/robust_benchmark.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CONFIG=$1
+CHECKPOINT=$2
+OUT=$3
+CORR=${CORR:-'fog'}
+SEV=${SEV:- 2 3}
+#CORR=${CORR:-'benchmark'}
+#SEV=${SEV:- 0 1 2 3 4 5}
+
+#pwd; ls -l  > dir_pwd.txt
+#ls -l $(dirname "$0") > dir_0.txt
+source /opt/bwhpc/common/devel/miniconda3/etc/profile.d/conda.sh
+conda activate cvproj
+
+python -Wignore tools/analysis_tools/test_robustness.py \
+    $CONFIG \
+    $CHECKPOINT \
+    --out=$OUT \
+    --corruptions=$CORR \
+    --eval bbox \
+    --launcher slurm \
+    --summaries True \
+    --severities=$SEV
---
+++ tools/slurm_robust-bm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -x
+
+PARTITION=$1
+JOB_NAME=$2
+CONFIG=$3
+CHECKPOINT=$4
+OUT=$5
+TIME=$6
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+SRUN_ARGS=${SRUN_ARGS:-""}
+PY_ARGS=${@:7}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+sbatch -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --time=${TIME} \
+    ${SRUN_ARGS} \
+    ./tools/robust_benchmark.sh ${CONFIG} ${CHECKPOINT} ${OUT} 
